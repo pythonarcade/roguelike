pythonarcade/roguelike

commit 4cfc560e3de6ac913603a59c7a08e680d92acf72

Gradually adds type hints (issue #1)
Some type hints added while reading the code.

source/util.py
@@ -1,7 +1,7 @@
 from constants import *
 
 
-def char_to_pixel(char_x, char_y):
+def char_to_pixel(char_x: int, char_y: int) -> (int, int):
     px = char_x * SPRITE_WIDTH * SPRITE_SCALE + SPRITE_WIDTH / 2 * SPRITE_SCALE
     py = (
         char_y * SPRITE_HEIGHT * SPRITE_SCALE + SPRITE_HEIGHT / 2 * SPRITE_SCALE
@@ -9,7 +9,7 @@ def char_to_pixel(char_x, char_y):
     return px, py
 
 
-def pixel_to_char(pixel_x, pixel_y):
+def pixel_to_char(pixel_x: int, pixel_y: int) -> (int, int):
     px = pixel_x - SPRITE_WIDTH / 2 * SPRITE_SCALE
     px = round(px / (SPRITE_WIDTH * SPRITE_SCALE))
 

source/map_to_sprites.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import arcade
 
 from constants import *
@@ -10,7 +12,7 @@ from entities.orc import Orc
 from entities.troll import Troll
 
 
-def map_to_sprites(game_map):
+def map_to_sprites(game_map: List[List[int]]) -> arcade.SpriteList[Entity]:
 
     sprite_list = arcade.SpriteList(use_spatial_hash=True, spatial_hash_cell_size=16)
 

source/procedural_generation/game_map.py
@@ -4,6 +4,8 @@ from constants import *
 
 
 # Some variables for the rooms in the map
+from entities.entity import Entity
+
 ROOM_MAX_SIZE = 10
 ROOM_MIN_SIZE = 6
 MAX_ROOMS = 35
@@ -67,7 +69,7 @@ def place_entities(room, entities, max_monsters_per_room, max_items_per_room):
 
 
 class GameMap:
-    def __init__(self, width, height):
+    def __init__(self, width: int, height: int):
         self.map_width = width
         self.map_height = height
         self.tiles = [
@@ -78,7 +80,7 @@ class GameMap:
         ]
 
     def make_map(
-        self, player,
+        self, player: Entity,
     ):
         rooms = []
         num_rooms = 0

source/recalculate_fov.py
@@ -1,15 +1,17 @@
 """
 Calculate Field Of Vision (FOV)
 """
+from typing import List
 
 import arcade
 import math
 
 from constants import *
+from entities.entity import Entity
 from util import char_to_pixel
 
 
-def recalculate_fov(char_x, char_y, radius, sprite_lists):
+def recalculate_fov(char_x: int, char_y: int, radius: int, sprite_lists: List[arcade.SpriteList[Entity]]):
     for sprite_list in sprite_lists:
         for sprite in sprite_list:
             if sprite.is_visible:

commit 64891ac47a6579092e26cc149c8d6b5811c42844

fixes typing mistake introduced in 4cfc560

source/util.py
@@ -1,7 +1,7 @@
 from constants import *
 
 
-def char_to_pixel(char_x: int, char_y: int) -> (int, int):
+def char_to_pixel(char_x: int, char_y: int) -> (float, float):
     px = char_x * SPRITE_WIDTH * SPRITE_SCALE + SPRITE_WIDTH / 2 * SPRITE_SCALE
     py = (
         char_y * SPRITE_HEIGHT * SPRITE_SCALE + SPRITE_HEIGHT / 2 * SPRITE_SCALE
@@ -9,7 +9,7 @@ def char_to_pixel(char_x: int, char_y: int) -> (int, int):
     return px, py
 
 
-def pixel_to_char(pixel_x: int, pixel_y: int) -> (int, int):
+def pixel_to_char(pixel_x: float, pixel_y: float) -> (int, int):
     px = pixel_x - SPRITE_WIDTH / 2 * SPRITE_SCALE
     px = round(px / (SPRITE_WIDTH * SPRITE_SCALE))
 