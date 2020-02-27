97cbd01e93d6197f39693c5deb9065db1063691e
Filip Lajszczak <filip.lajszczak@gmail.com>
2020-02-27 18:37:21 +0000

    load textures from a filelist

--- source/themes/custom_1_textures.py
+++ source/themes/custom_1_textures.py
@@ -5,15 +5,17 @@ from themes.current_theme import *
 
 assets_path = pathlib.Path(__file__).resolve().parent / "custom_1"
 
-# Load  the textures our sprites use on game start-up.
-textures = []
+filenames = [
+    "floor.png",
+    "player.png",
+    "orc.png",
+    "troll.png",
+    "wall.png",
+    "red_potion.png",
+    "scroll.png",
+    "dead_body.png",
+    "stairs_down.png",
+]
 
-textures.append(load_texture(assets_path / "floor.png"))
-textures.append(load_texture(assets_path / "player.png"))
-textures.append(load_texture(assets_path / "orc.png"))
-textures.append(load_texture(assets_path / "troll.png"))
-textures.append(load_texture(assets_path / "wall.png"))
-textures.append(load_texture(assets_path / "red_potion.png"))
-textures.append(load_texture(assets_path / "scroll.png"))
-textures.append(load_texture(assets_path / "dead_body.png"))
-textures.append(load_texture(assets_path / "stairs_down.png"))
+# Load  the textures our sprites use on game start-up.
+textures = [load_texture(str(assets_path / filename)) for filename in filenames]
