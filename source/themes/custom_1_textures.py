d4e9f83b3a39e69d61306984c5b64d5b636669b6
Filip Lajszczak <filip.lajszczak@gmail.com>
2020-02-27 18:30:57 +0000

    fixes tests to match changed code

--- source/themes/custom_1_textures.py
+++ source/themes/custom_1_textures.py
@@ -1,15 +1,19 @@
+import pathlib
+
 from arcade import load_texture
 from themes.current_theme import *
+
+assets_path = pathlib.Path(__file__).resolve().parent / "custom_1"
 
 # Load  the textures our sprites use on game start-up.
 textures = []
 
-textures.append(load_texture("themes/custom_1/floor.png"))
-textures.append(load_texture("themes/custom_1/player.png"))
-textures.append(load_texture("themes/custom_1/orc.png"))
-textures.append(load_texture("themes/custom_1/troll.png"))
-textures.append(load_texture("themes/custom_1/wall.png"))
-textures.append(load_texture("themes/custom_1/red_potion.png"))
-textures.append(load_texture("themes/custom_1/scroll.png"))
-textures.append(load_texture("themes/custom_1/dead_body.png"))
-textures.append(load_texture("themes/custom_1/stairs_down.png"))
+textures.append(load_texture(assets_path / "floor.png"))
+textures.append(load_texture(assets_path / "player.png"))
+textures.append(load_texture(assets_path / "orc.png"))
+textures.append(load_texture(assets_path / "troll.png"))
+textures.append(load_texture(assets_path / "wall.png"))
+textures.append(load_texture(assets_path / "red_potion.png"))
+textures.append(load_texture(assets_path / "scroll.png"))
+textures.append(load_texture(assets_path / "dead_body.png"))
+textures.append(load_texture(assets_path / "stairs_down.png"))
